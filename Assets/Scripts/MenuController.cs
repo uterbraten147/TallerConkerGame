92ec0f6b821f2e075e792aeccb8caed5651bd4ca
uterbraten147 <147and@gmail.com>
2019-10-16 14:13:17 -0500

    menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+   // public gameObject MainPanel, CreditsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // MainPanel.setActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
