890bc2e48c41c303094b920c383882980d19df23
RobertVid <33789155+RobertVid@users.noreply.github.com>
2019-10-17 15:55:45 -0500

    Enemigo

---
+++ Assets/Scripts/Bala.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    private Rigidbody rigBala;
+    public float velocidadBala;
+    public float damage;
+    void Start()
+    {
+        rigBala = GetComponent<Rigidbody>();
+        rigBala.velocity = velocidadBala*transform.forward;
+        Destroy(gameObject, 5);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SendMessage("ReciveDamege", damage,SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    private bool followingPlayer=false;
+    private GameObject positionPlayer;
+    public float outTarget;
+    public GameObject balaPFB;
+    public float fireRate;
+    private float lastTimeShoot;
+    void Start()
+    {
+        //agent.SetDestination(target.transform.position);
+    }
+
+    void Update()
+    {
+        if (followingPlayer) {
+            float distancia = Vector3.Distance(transform.position, positionPlayer.transform.position);
+            //Debug.Log(distancia);
+            if (distancia > outTarget)
+            {
+                agent.SetDestination(transform.position);
+                followingPlayer = false;
+            }
+            else {
+                if (Time.time > lastTimeShoot + fireRate) {
+                    lastTimeShoot = Time.time;
+                    Instantiate(balaPFB,transform.position,transform.rotation);
+                }
+            }
+            
+        }
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            agent.SetDestination(other.gameObject.transform.position);
+            positionPlayer = other.gameObject;
+            followingPlayer = true;
+        }
+    }
+
+}
