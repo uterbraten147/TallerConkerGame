cd009c904949224c19bf490d6e358e3254cdc131
uterbraten147 <147and@gmail.com>
2019-10-17 23:46:10 -0500

    playermovement

--- Assets/Scripts/GamePManager.cs
+++ Assets/Scripts/GamePManager.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class GamePManager : MonoBehaviour
 {
-   
-   // public gameObject MainPanel, CreditsPanel;
     // Start is called before the first frame update
     void Start()
     {
-       // MainPanel.setActive(true);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         
     }
 }
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    
+    public float speed = 20;
+  
+   
+   private  void Update()
+    {
+        movement();
+    }
+
+
+    void movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 MovePlayer = new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+        transform.Translate(MovePlayer, Space.Self);
+    }
+}
