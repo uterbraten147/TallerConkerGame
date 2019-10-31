16a25470f67c780eb8252286fca6139e646a3b5f
RobertVid <33789155+RobertVid@users.noreply.github.com>
2019-10-30 22:49:20 -0600

    Ai enemy
    Algunas correciones

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -13,33 +13,47 @@ public class Enemy : MonoBehaviour
     public GameObject balaPFB;
     public float fireRate;
     private float lastTimeShoot;
+    private Vector3 point;
     void Start()
     {
         //agent.SetDestination(target.transform.position);
+        CambiarPunto();
     }
 
     void Update()
     {
-        if (followingPlayer) {
+        if (followingPlayer)
+        {
             float distancia = Vector3.Distance(transform.position, positionPlayer.transform.position);
             //Debug.Log(distancia);
             if (distancia > outTarget)
             {
-                agent.SetDestination(transform.position);
+                //agent.SetDestination(transform.position);
+                CambiarPunto();
                 followingPlayer = false;
             }
-            else {
-                if (Time.time > lastTimeShoot + fireRate) {
+            else
+            {
+                if (Time.time > lastTimeShoot + fireRate)
+                {
                     lastTimeShoot = Time.time;
-                    Instantiate(balaPFB,transform.position,transform.rotation);
+                    Instantiate(balaPFB, transform.position, transform.rotation);
                 }
-                Vector3 direccion =target.transform.position-transform.position;
+                Vector3 direccion = target.transform.position - transform.position;
                 Quaternion Rotacion = Quaternion.LookRotation(direccion);
                 transform.rotation = Quaternion.Slerp(transform.rotation, Rotacion, 8.0f * Time.deltaTime);
             }
+        }
+        else
+        {
+            float distanciaAutoma = Vector3.Distance(transform.position, point);
+          
+            if (distanciaAutoma<=3)
+            {
+                CambiarPunto();
+            }
             
         }
-        
     }
     private void OnTriggerStay(Collider other)
     {
@@ -51,9 +65,32 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        
+    public float range = 10.0f;
+
+    void CambiarPunto() {
+        bool Enontrado = true;
+        while (Enontrado) {
+            if (RandomPoint(transform.position, range, out point))
+            {
+                agent.SetDestination(point);
+                Enontrado = false;
+            }
+        }
     }
 
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
 }
