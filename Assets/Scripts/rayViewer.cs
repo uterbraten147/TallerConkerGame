f33764c2e92352135c146bb275af94037cf9cf61
uterbraten147 <147and@gmail.com>
2019-11-02 19:07:29 -0600

    mmm

---
+++ Assets/Scripts/rayViewer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayViewer : MonoBehaviour
+{
+	public float weaponRange = 50f;
+	public Camera  cam;
+  
+    void Start()
+	{
+		cam= GetComponent<Camera>();
+        
+    }
+
+
+    void Update()
+    {
+	     Vector3 lineOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f,0.5f,0));
+	     Debug.DrawRay(lineOrigin, cam.transform.forward * weaponRange, Color.green);
+    }
+}
